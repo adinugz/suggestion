71dfb39462ba0616d7d02a1d448039e4ad17b776
Adi Nugroho <91783633+adinugz@users.noreply.github.com>
2023-02-25 18:51:56 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Berbalas Komentar Live</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="script.js"></script>
+</head>
+<body>
+	<div id="komentar">
+		<h2>Kolom Komentar</h2>
+		<form id="form-komentar" method="post">
+			<label for="nama">Nama:</label>
+			<input type="text" id="nama" name="nama" required>
+			<label for="komentar">Komentar:</label>
+			<textarea id="komentar" name="komentar" required></textarea>
+			<input type="submit" value="Kirim">
+		</form>
+		<div id="daftar-komentar"></div>
+	</div>
+</body>
+</html>
